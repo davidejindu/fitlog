e75c16acee0ee1d4ca97572bac74aab3dfce1f27
David Ejindu <ejindudavid540@gmail.com>
2025-08-01 19:51:18 -0400

    fix cors error

--- backend/src/main/java/com/ejindu/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/ejindu/backend/config/SecurityConfig.java
@@ -41,10 +41,13 @@ public class SecurityConfig {
         @Bean
         public CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration configuration = new CorsConfiguration();
-                configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:5173", "http://localhost:3000"));
+                configuration.setAllowedOriginPatterns(Arrays.asList(
+                                "https://fitlog-iota.vercel.app"));
                 configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                 configuration.setAllowedHeaders(Arrays.asList("*"));
+                configuration.setExposedHeaders(Arrays.asList("Authorization", "Content-Type"));
                 configuration.setAllowCredentials(true);
+                configuration.setMaxAge(3600L);
 
                 UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
                 source.registerCorsConfiguration("/**", configuration);
